f13032960403fef081af230ca21ed66422553c81
alghzzawi <mohammadalghazzawi9@gmail.com>
2023-05-07 15:56:04 +0300

    first commit

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -43,25 +43,7 @@ class UsersTable extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                <label>
-                                    <div className="usersName">
-                                        <input type="radio" name="option" value="Option 1" />
-                                        <img alt="userProfile" src={profile} />
-                                        <div className="name_email">
-                                            <p>Cortney Heny</p>
-                                            <p>email</p>
-                                        </div>
-                                    </div>
-                                </label>
-                            </td>
-                            <td>Design 53</td>
-                            <td>0/12</td>
-                            <td>4/7</td>
-                            <td>2/3</td>
-                            <td>80/100</td>
-                        </tr>
+                        
                         {this.state.users.map(itme => {
                             return (
                                 <tr>
